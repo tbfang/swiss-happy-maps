2d13c63ed1202f95a5d8fa7b3d38f2474040c8d7
Kirtan Padh <kirtan.701@gmail.com>
2017-01-04 15:47:15 +0100

    Runs on the cluster now. See notes for details

--- run_count_sent.py
+++ run_count_sent.py
@@ -1,9 +1,14 @@
 import json
 import os
 import glob
+from pyspark import SparkConf, SparkContext
 
-def count_sentiment(file):
-    rdd = sc.wholeTextFiles(file).values()
+
+conf = SparkConf().setAppName("ADA-gcl")
+sc = SparkContext(conf=conf)
+
+def count_sentiment(p):
+    rdd = sc.wholeTextFiles(p).values()
     parsed = rdd.map(json.loads)
     m=parsed.top(1)
     no_sentiment=0
@@ -16,17 +21,17 @@ def count_sentiment(file):
     return no_sentiment, sentiment
 
 #path = os.getcwd() +'/datasets'
-path = '/datasets/goodcitylife/'
+pathe = 'hdfs://iccluster046.iccluster.epfl.ch:8020/datasets/goodcitylife/april/harvest3r_instagram_data_28-04_0.json'
 instagram_files='/harvest3r_instagram_data*.json'
 news_files='/harvest3r_news_data*.json'
 twitter_files='/harvest3r_twitter_data*.json'
 count_wo_sentiment=0
 count_w_sentiment=0
-for folder in os.listdir(path):
-    for file in glob.glob(os.path.join(path,folder + instagram_files)):
-        print(file)
-        no_sent,sent=count_sentiment(file)
-        count_wo_sentiment+=no_sent
-        count_w_sentiment+=sent
+#for folder in os.listdir(path):
+#    for file in glob.glob(os.path.join(path,folder + instagram_files)):
+#        print(file)
+no_sent,sent=count_sentiment(pathe)
+count_wo_sentiment=no_sent
+count_w_sentiment=sent
 print('Posts w/o sentiment: %d' % count_wo_sentiment)
 print('Posts with sentiment: %d' % count_w_sentiment)
