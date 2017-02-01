bc4517fd19301da393d8f83d3e3f501006cee9c7
lemmmr <luis.medinarios@epfl.ch>
2017-02-01 16:34:31 +0100

    Commented Machine Learning part as well as the function to get the fasttext text files - Luis Medina

--- Scripts/get_fasttext_format.py
+++ Scripts/get_fasttext_format.py
@@ -8,13 +8,16 @@ from pyspark.sql import SQLContext
 from pyspark.sql.functions import lit
 import pandas as pd
 
+#This function looks for all the instagram posts which have a sentiment so that we can generate both a training data set
+#(instagram posts with labels) and a test data set (instagram posts without labels), this function also prepares those data 
+#sets to be in the specific format in order to be processed by FastText and could predict the labels for the test data set.
+
 conf = SparkConf().setAppName("ADA-gcl")
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
 
 # Path definitions
-#If you try to run it for the whole dataset, it gives an out of memory error
 base_path = 'hdfs://iccluster046.iccluster.epfl.ch:8020/datasets/goodcitylife/'
 insta_files = '*/harvest3r_instagram_data*.json'
 news_files = '*/harvest3r_news_data*.json'
@@ -67,16 +70,17 @@ def get_paths(dir_path, recurse):
         files.append(l.split(' ')[-1].rstrip())
     return(files)
 
+#First we are going to get all the paths of the json files in the cluster (news, twitter and instagram information)
 all_paths = get_paths(base_path, True)
 
-#months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october']
-#for m in months:
+#Secondly, we are going to get just all the instagram files.
 file_list = []
 for path in all_paths:
     if(fnmatch.fnmatch(path, insta_files)):
         file_list.append(path)
 
-
+#Finally, we are going to get the sentiment for the instagram post in order to generate both the train data set and the test data set
+#this by reading each of the json files for instagram.
 for f in file_list:
     df_temp = sqlContext.read.json(f).collect()
     writeCols(df_temp)
